d35f13c77656e49ae27d58761c56424f85c5f0d2
sungbin <dusvlf5950@naver.com>
2025-12-08 20:35:33 +0900

    fix: Vite 설정에서 미리보기 호스트 및 포트 주석 처리

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    host: true,
-    port: 4173,
+    // host: true,
+    // port: 4173,
     allowedHosts: ['apiwiki.supabin.com'],
   },
 })
