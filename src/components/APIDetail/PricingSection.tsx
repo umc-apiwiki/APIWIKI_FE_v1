05cf360e56d4366a65ab6b5cedfb857067b17fa0
현영 <hyun0@mail.ulsan.ac.kr>
2026-01-28 20:34:14 +0900

    feat: api 상세 페이지 구현
    - api 상세 페이지 라우트 (/apis/:id) 추가
    - api card 클릭 시 상세 페이지 연결
    - 상세 페이지 탭 메뉴 및 섹션 전환 구현
    - Layout.tsx에서 배경 원 위치를 헤더 높이에 맞게 아래로 재조정

---
+++ src/components/APIDetail/PricingSection.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function PricingSection() {
+  return <div>PricingSection</div>
+}
---
+++ src/components/APIDetail/ReviewSection.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ReviewSection() {
+  return <div>ReviewSection</div>
+}
